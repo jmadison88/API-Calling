636e5a8f613f8469b1262bddb9f4a04fc931c60b
jmadison <jmad99@sbcglobal.net>
2023-03-02 10:32:40 -0600

    Finished view for info on creatures

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var entries = [Entry]()
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(entries) { entry in
+                VStack(alignment: .leading) {
+                    Link(destination: URL(string: entry.image)!) {
+                        Text(entry.name)
+                    }
+                    Text(entry.description)
+                    Text(entry.cooking_effect).bold()
+                }
+            }
+            .navigationTitle("Hyrule Creatures (No Drops)")
         }
-        .padding()
+        .task {
+            await getData()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+            message: Text("There was a problem loading the Hyrule Creatures"),
+                  dismissButton: .default(Text("OK")))
+        }
+    }
+    func getData() async {
+        let query = "https://botw-compendium.herokuapp.com/api/v2/category/creatures"
+        if let url = URL(string: query) {
+            if let (data, _) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode(Data.self, from: data) {
+                    entries = decodedResponse.data.food
+                    return
+                }
+            }
+        }
+        showingAlert = true
     }
 }
 
@@ -23,4 +50,27 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Entry: Identifiable, Codable {
+    var id = UUID()
+    var cooking_effect: String
+    var description: String
+    var image: String
+    var name: String
+    
+    private enum CodingKeys : String, CodingKey {
+        case cooking_effect
+        case description
+        case image
+        case name
+    }
+}
+
+struct Data: Codable {
+    var data: Food
+}
+
+struct Food: Codable {
+    var food: [Entry]
 }
